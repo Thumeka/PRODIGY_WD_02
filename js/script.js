e9230f403a7c98199974f4f067f0beb75456b725
thumeka <thumekatembani21@gmail.com>
2024-08-22 14:07:54 +0200

    When the play button is clicked, it triggers the play function.

--- js/script.js
+++ js/script.js
@@ -1,56 +1,115 @@
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let timeRef = document.querySelector(".timer-display");
-let int = null;
-let lapCount = 1;
-let lapList = document.getElementById("lap-list");
+const playBtn = document.getElementsByClassName("playbtn")[0];
+const lapBtn = document.getElementsByClassName("lapbtn")[0];
+const resetBtn = document.getElementsByClassName("resetbtn")[0];
+const lapS = document.getElementsByClassName("laps")[0];
+const clearBtn = document.getElementsByClassName("clearbtn")[0];
 
-document.getElementById("start-timer").addEventListener("click", () => {
-  if (int !== null) {
-    clearInterval(int);
-  }
-  int = setInterval(displayTimer, 10);
-});
 
-document.getElementById("pause-timer").addEventListener("click", () => {
-  clearInterval(int);
-});
+const innerShape = document.getElementsByClassName("inner-shape")[0];
+const sec = document.getElementsByClassName("sec")[0];
+const msec = document.getElementsByClassName("msec")[0];
+const min = document.getElementsByClassName("min")[0];
 
-document.getElementById("reset-timer").addEventListener("click", () => {
-  clearInterval(int);
-  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-  timeRef.innerHTML = "00 : 00 : 00 : 000 ";
-});
-document.getElementById("lap-timer").addEventListener("click", () => {
-  let lapTime = timeRef.innerHTML;
-  let lapItem = document.createElement("li");
-  lapItem.innerText = `Lap ${lapCount}: ${lapTime}`;
-  lapList.appendChild(lapItem);
-  lapCount++;
-});
-function displayTimer() {
-  milliseconds += 10;
-  if (milliseconds == 1000) {
-    milliseconds = 0;
-    seconds++;
-    if (seconds == 60) {
-      seconds = 0;
-      minutes++;
-      if (minutes == 60) {
-        minutes = 0;
-        hours++;
+let secCounter = 0;
+let secs;
+let msecCounter = 0;
+let msecs;
+let minCounter = 0;
+let mins;
+let lapItem = 0; 
+
+const toggleBtn = () => {
+  lapBtn.classList.add("visibility");
+  resetBtn.classList.add("visibility");
+};
+
+const play = () => {
+  toggleBtn();
+  if (playBtn.innerHTML === "Play") {
+    playBtn.innerHTML = "Pause";
+
+    mins = setInterval(() => {
+      min.innerHTML = `${++minCounter} :`;
+      if (minCounter < 10) {
+        min.innerHTML = `0${minCounter} :`;
       }
-    }
+    }, 60 * 1000);
+
+    secs = setInterval(() => {
+      if (secCounter === 59) {
+        secCounter = 1;
+      }
+      sec.innerHTML = `${++secCounter} :`;
+      if (secCounter < 10) {
+        sec.innerHTML = `0${secCounter} :`;
+      }
+    }, 1000);
+
+    msecs = setInterval(() => {
+      if (msecCounter === 99) {
+        msecCounter = 1;
+      }
+      msec.innerHTML = `${++msecCounter}`;
+      if (msecCounter < 10) {
+        msec.innerHTML = `0${msecCounter}`;
+      }
+    }, 10);
+  } else {
+    playBtn.innerHTML = "Play";
+    clearInterval(mins);
+    clearInterval(secs);
+    clearInterval(msecs);
   }
+  if (playBtn.innerHTML === "Pause") {
+    lapBtn.classList.remove("visibility");
+    resetBtn.classList.remove("visibility");
+  }
+};
 
-  let h = hours < 10 ? "0" + hours : hours;
-  let m = minutes < 10 ? "0" + minutes : minutes;
-  let s = seconds < 10 ? "0" + seconds : seconds;
-  let ms =
-    milliseconds < 10
-      ? "00" + milliseconds
-      : milliseconds < 100
-      ? "0" + milliseconds
-      : milliseconds;
+const reset = () => {
+  clearInterval(mins);
+  clearInterval(secs);
+  clearInterval(msecs);
 
-  timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+  minCounter = 0;
+  secCounter = 0;
+  msecCounter = 0;
+
+  min.innerHTML = "00 :";
+  sec.innerHTML = "00 :";
+  msec.innerHTML = "00";
+
+  playBtn.innerHTML = "Play";
+  lapBtn.classList.add("visibility");
+  resetBtn.classList.add("visibility");
+  
+};
+
+const lap = () => {
+  const li = document.createElement("li");
+  const number = document.createElement("span");
+  const timeStamp = document.createElement("span");
+
+  li.setAttribute("class", "lap-item");
+  number.setAttribute("class", "number");
+  timeStamp.setAttribute("class", "time-stamp");
+
+  number.innerHTML = `#${++lapItem}`
+  timeStamp.innerHTML = `${minCounter} :${secCounter} :${msecCounter}`;
+  
+  li.append(number, timeStamp);
+  lapS.append(li);
+  clearBtn.classList.remove("laptime")
+};
+
+const clear = () => {
+  lapS.innerHTML = ""
+  lapS.append(clearBtn)
+  clearBtn.classList.add("laptime")
+
 }
+
+playBtn.addEventListener("click", play);
+lapBtn.addEventListener("click", lap);
+resetBtn.addEventListener("click", reset);
+clearBtn.addEventListener("click", clear)
